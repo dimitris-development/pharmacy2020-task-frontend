879c1b4a59baeb2b20d95159b4f272cfc03c24c3
James C <dimitrios.collier@gmail.com>
2020-12-30 01:53:48 +0200

    Created reducer folder and corresponding reducers.

---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+export userAuthReducer from '../reducers/user.auth.reducer'
+export userInfoReducer from '../reducers/user.info.reducer'
+export errorUserReducer from '../reducers/error.user.reducer'
